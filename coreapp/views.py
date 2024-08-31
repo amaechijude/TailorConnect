bcc890d91cc8070b6bf06408d942102f29082005
amaechijude <ugwuamaechi5@gmail.com>
2024-08-31 18:11:46 +0100

    fix

--- coreapp/views.py
+++ coreapp/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import *
+from django.core.mail import send_mail
+
 
 
 def index(request):
     designers = Designer.verified.all()
     styles = Style.published.all()
+    category = Category.objects.all()
     context = {
         "designers": designers,
-        "styles": styles
+        "styles": styles,
+        'category': category,
     }
     return render(request, 'core/index.html', context)
+
+
+def designer(request, pk):
+    ds = Designer.verified.get(id=pk)
+    return render(request, 'core/designer.html', {'ds':ds})
+
+
+def category(request, pk):
+    cat = Category.objects.filter(id=pk)
+    return render(request, 'core/category.html', {'cat': cat})
+
+
+
