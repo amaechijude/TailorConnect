755017c9ef378bba0524c53fb23bbc101171b715
amaechijude <ugwuamaechi5@gmail.com>
2024-09-02 22:34:02 +0100

    battery low

--- authUserApp/forms.py
+++ authUserApp/forms.py
@@ -1,14 +1,14 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={"placeholder": "password1"}))
-    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={"placeholder": "Confirm password1"}))
+class RegisterForm(forms.Form):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={"id":"email", "class":"form-control","placeholder":"Enter your Email"}))
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={"id":"subject", "class":"form-control", "placeholder": "password1"}))
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(attrs={"id":"subject", "class":"form-control", "placeholder": "Confirm password1"}))
 
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(required=True)
-    password= forms.CharField(required=True, widget=forms.PasswordInput(attrs={"placeholder": "Enter password"}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={"id":"email", "class":"form-control","placeholder":"Enter your Email"}))
+    password= forms.CharField(required=True, widget=forms.PasswordInput(attrs={"id":"subject", "class":"form-control", "placeholder": "Enter password"}))
 
