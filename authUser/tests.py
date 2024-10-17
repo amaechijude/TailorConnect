42b1cf38021fb8ddb80a4170b932c10edb181198
amaechijude <ugwuamaechi5@gmail.com>
2024-10-17 15:16:22 +0100

    initiate test

--- authUser/tests.py
+++ authUser/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
+from .forms import RegisterForm, LoginForm, ShippingAddressForm, MeasurementForm
+from .models import User, ShippingAddress, WishList, Measurement
+from .views import *
 
 # Create your tests here.
